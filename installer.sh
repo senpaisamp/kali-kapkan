5d46f7572f6492774774158d84b7c9fe92260cab
senpaisamp <98622787+senpaisamp@users.noreply.github.com>
2024-04-12 10:58:01 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,30 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt-get purge nikto crackmapexec 
+#!/bin/bash
 
-sudo apt-get install -y flameshot arc-theme i3status i3 python3-pip rofi cargo compton imagemagick tmux picom alacritty nitrogen
+sudo apt update && sudo apt upgrade -y
+
+sudo apt-get install -y wget curl git thunar
+sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick alacritty
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
+sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
+
+mkdir -p ~/.local/share/fonts/
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
+
+unzip Iosevka.zip -d ~/.local/share/fonts/
+unzip RobotoMono.zip -d ~/.local/share/fonts/
+
+fc-cache -fv
+
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps && mkdir -p build && cd build && meson ..
+ninja
+sudo ninja install
+cd ../..
+
+pip3 install --user pywal
+export PATH="${PATH}:${HOME}/.local/bin/"
 
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/compton
@@ -10,16 +32,17 @@ mkdir -p ~/.config/rofi
 mkdir -p ~/.config/alacritty
 touch ~/.tmux.conf
 
-pip3 install --user pywal
-export PATH="${PATH}:${HOME}/.local/bin/"
-
 cp .config/i3/config ~/.config/i3/config
 cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-cp .config/i3/i3status.conf ~/.config/i3/i3status.conf
+cp .config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
+cp .config/compton/compton.conf ~/.config/compton/compton.conf
 cp .config/rofi/config ~/.config/rofi/config
+cp .fehbg ~/.fehbg
+cp .config/i3/clipboard_fix.sh ~/.config/i3/clipboard_fix.sh
+cp -r .wallpaper ~/.wallpaper 
 cp .config/.tmux.conf ~/.tmux.conf
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "fatto!
+echo "ricordati di modificare icone e cose varie con lxappearancee seleziona arc-dark"
 
-sudo apt update && sudo apt upgrade -y
-sudo apt-get purge nikto crackmapexec -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
